2d7c8057affbf43b628d2734552a4f08ff5fc955
heliumhydride <34401203+heliumhydride@users.noreply.github.com>
2024-10-23 17:21:08 +0200

    add function to read file into a buffer

--- config.h
+++ config.h
@@ -9,5 +9,6 @@ static const int v_padding = 1; // horizontal character padding (==space between
 static const int h_padding = 2; // same but vertical
 
 static const char* default_translation = "en_nkjv";
+static const char* bible_directory = "/usr/share/bibl/"; // On Windows, could be set to "C:/Program Files/bibl/share/bibl" ?
 
 #endif
